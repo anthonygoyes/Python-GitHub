109b202c1e63b976ceaaf1837983aa002b27424b
Elliot <elliot.cazar11@gmail.com>
2023-10-02 19:43:32 -0500

    Interfaz Menu

---
+++ src/InterfazMenu.py
@@ -0,0 +1,9 @@
+def menu():
+    print("=====Menu=====")
+    print("1) Suma")
+    print("2) Resta")
+    print("3) Multiplicacion")
+    print("4) Divicion")
+    print("5) Potencia")
+    print("6) Raiz Cuadrada")
+    print("7) Factorial")
