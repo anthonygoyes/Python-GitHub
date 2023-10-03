0bb8a231ecff1e4a693704138dba47753d5c9896
Elliot <elliot.cazar11@gmail.com>
2023-10-02 20:48:22 -0500

    agregando el main

--- src/main.py
+++ src/main.py
@@ -1,1 +1,27 @@
-#main
+from InterfazMenu import menu
+
+def main():
+    while True:
+        opcion = menu()
+        if opcion==1:
+            #1
+        elif opcion==2:
+            #opcion2
+        elif opcion==3:
+            #opcion3
+        elif opcion==4:
+            #opcion4
+        elif opcion==5:
+            #opcion5
+        elif opcion==6:
+            #opcion6
+        elif opcion==7:
+            #opcion7
+        elif opcion==8:
+            break #Salir del while
+        else:
+            print("Opcion no valida")
+            
+
+if __name__ == "__main__":
+    main()
