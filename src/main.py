7587aa75bbee9e0164c4c187e02dabac6bba3c9f
anthonygoyes <anthonygoyes13@gmail.com>
2023-10-02 21:11:03 -0500

    main

--- src/main.py
+++ src/main.py
@@ -1,27 +1,16 @@
 from InterfazMenu import menu
+import  DefAritmeticas as df
 
-def main():
-    while True:
-        opcion = menu()
-        if opcion==1:
-            #1
-        elif opcion==2:
-            #opcion2
-        elif opcion==3:
-            #opcion3
-        elif opcion==4:
-            #opcion4
-        elif opcion==5:
-            #opcion5
-        elif opcion==6:
-            #opcion6
-        elif opcion==7:
-            #opcion7
-        elif opcion==8:
-            break #Salir del while
-        else:
-            print("Opcion no valida")
-            
 
-if __name__ == "__main__":
-    main()
+while True:
+    opcion = menu()
+    if opcion==1:
+        num1=int(input("Primer numero "))
+        num2=int(input("Segundo numero "))
+        print("El resultado es ", df.suma(num1, num2))
+    elif opcion==8:
+        break #Salir del while
+    else:
+        print("Opcion no valida")
+        
+
